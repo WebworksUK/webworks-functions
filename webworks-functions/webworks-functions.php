e024fa6c918bb8dd4b4a40f17a479bda066cce0b
WebworksJoe <joe@webworks.london>
2017-03-02 09:58:36 +0000

    Removed script: OPTIMISE WOO SCRIPTS TO ONLY LOAD ON WOO PAGES
    Woo scripts required for cart dropdown functionality on non-woo pages

--- webworks-functions/webworks-functions.php
+++ webworks-functions/webworks-functions.php
@@ -8,39 +8,6 @@
 * Author URI: https://webworks.london
 */
 
-//OPTIMISE WOO SCRIPTS TO ONLY LOAD ON WOO PAGES
-add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_styles', 99 );
-function child_manage_woocommerce_styles() {
-//remove generator meta tag
-remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
-//first check that woo exists to prevent fatal errors
-if ( function_exists( 'is_woocommerce' ) ) {
-//dequeue scripts and styles
-if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
-wp_dequeue_style( 'woocommerce_frontend_styles' );
-wp_dequeue_style( 'woocommerce_fancybox_styles' );
-wp_dequeue_style( 'woocommerce_chosen_styles' );
-wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
-wp_dequeue_script( 'wc_price_slider' );
-wp_dequeue_script( 'wc-single-product' );
-wp_dequeue_script( 'wc-add-to-cart' );
-wp_dequeue_script( 'wc-cart-fragments' );
-wp_dequeue_script( 'wc-checkout' );
-wp_dequeue_script( 'wc-add-to-cart-variation' );
-wp_dequeue_script( 'wc-single-product' );
-wp_dequeue_script( 'wc-cart' );
-wp_dequeue_script( 'wc-chosen' );
-wp_dequeue_script( 'woocommerce' );
-wp_dequeue_script( 'prettyPhoto' );
-wp_dequeue_script( 'prettyPhoto-init' );
-wp_dequeue_script( 'jquery-blockui' );
-wp_dequeue_script( 'jquery-placeholder' );
-wp_dequeue_script( 'fancybox' );
-wp_dequeue_script( 'jqueryui' );
-}
-}
-}
-
 //Load custom login CSS
 function my_custom_login() {
     echo '<link rel="stylesheet" id="ww-admin-css" type="text/css" href="' . plugin_dir_url( __FILE__ ) . 'login/custom-login-styles.css">';
